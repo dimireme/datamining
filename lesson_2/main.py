87d1871ff904d5673e4c2153ea138e678ee85609
Александр Шадрин <shadrinaa@altarix.ru>
2020-01-17 15:31:50 +0300

    Try to start task for lesson 2.

--- lesson_2/main.py
+++ lesson_2/main.py
@@ -11,3 +11,26 @@
 # Обязательно теги и автор должны существовать отдельными таблицами,
 # и должны быть корректно реализованы соответсвующие связи.
 
+from bs4 import BeautifulSoup
+import requests
+import time
+
+
+def get_data(url: str, params: dict) -> dict:
+    while True:
+        time.sleep(1)
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            break
+    return response.text
+
+
+url = "https://geekbrains.ru/posts"
+text = get_data(url, {})
+
+soup = BeautifulSoup(text, 'html.parser')
+# inner_html = soup.find('span',  class_="total-users").string
+# count = re.search(r'\d[\d\s]+\d', inner_html).group().replace(' ', '')
+# print(count)
+
+print(1)
